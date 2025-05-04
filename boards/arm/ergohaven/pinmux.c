18512255971ad701b27efd92b3ce2f9c2fd50b00
Danil Tolkachev <dandur.oak@gmail.com>
2025-05-04 16:59:06 +0300

    rename board

--- boards/arm/ergohaven/pinmux.c
+++ boards/arm/ergohaven/pinmux.c
@@ -11,11 +11,11 @@
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/devicetree.h>
 
-static int pinmux_nrfmicro_init(void) {
-#if (CONFIG_BOARD_NRFMICRO_13 || CONFIG_BOARD_NRFMICRO_13_52833)
+static int pinmux_ergohaven_init(void) {
+#if (CONFIG_BOARD_ERGOHAVEN)
     const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 #endif
     return 0;
 }
 
-SYS_INIT(pinmux_nrfmicro_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+SYS_INIT(pinmux_ergohaven_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
